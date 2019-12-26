9381be84ffd33a7852fce9f5e37cb55b40d687ee
Kazim Rizvi <i.kazimrizvi@gmail.com>
2019-12-26 23:25:22 +0530

    firstCommit

---
+++ app/src/main/java/com/example/roadresq/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.roadresq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //relativeLayout = findViewById(R.id.relative_main);
+    }
+
+
+}
